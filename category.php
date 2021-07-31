722d3b66a08698794737bbdfe493dcb86e5ed0a7
Victor Do <piousbox@gmail.com>
2021-07-31 20:32:09 +0000

    better formatting of category.php

--- category.php
+++ category.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying categories, under Technique
  * @link https://codex.wordpress.org/Template_Hierarchy
@@ -8,56 +9,56 @@ get_header(); ?>
 
 <div class="wrap">
 
-	<?php if ( have_posts() ) : ?>
-		<header class="page-header">
-			<?php
-				the_archive_title( '<h1 class="page-title" >', '</h1>' );
-				the_archive_description( '<div class="taxonomy-description">', '</div>' );
-			?>
-		</header><!-- .page-header -->
-	<?php endif; ?>
+  <?php if (have_posts()) : ?>
+    <header class="page-header">
+      <?php
+      the_archive_title('<h1 class="page-title" >', '</h1>');
+      the_archive_description('<div class="taxonomy-description">', '</div>');
+      ?>
+    </header><!-- .page-header -->
+  <?php endif; ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) :
-		?>
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+      <?php
+      if (have_posts()) :
+      ?>
+      <?php
+        /* Start the Loop */
+        while (have_posts()) :
+          the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/post/content', get_post_format() );
+          /*
+           * Include the Post-Format-specific template for the content.
+           * If you want to override this in a child theme, then include a file
+           * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+           */
+          get_template_part('template-parts/post/content', get_post_format());
 
-			endwhile;
+        endwhile;
 
-			the_posts_pagination(
-				array(
-					'prev_text'          => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-					'next_text'          => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-				)
-			);
+        the_posts_pagination(
+          array(
+            'prev_text'          => twentyseventeen_get_svg(array('icon' => 'arrow-left')) . '<span class="screen-reader-text">' . __('Previous page', 'twentyseventeen') . '</span>',
+            'next_text'          => '<span class="screen-reader-text">' . __('Next page', 'twentyseventeen') . '</span>' . twentyseventeen_get_svg(array('icon' => 'arrow-right')),
+            'before_page_number' => '<span class="meta-nav screen-reader-text">' . __('Page', 'twentyseventeen') . ' </span>',
+          )
+        );
 
-		else :
+      else :
 
-			get_template_part( 'template-parts/post/content', 'none' );
+        get_template_part('template-parts/post/content', 'none');
 
-		endif;
-		?>
+      endif;
+      ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    </main><!-- #main -->
+  </div><!-- #primary -->
   <?php get_sidebar(); ?>
 
   <aside id="secondary" class="widget-area" role="complementary" aria-label="Blog Sidebar">
-    <section id="recent-posts-2" class="widget widget_recent_entries">		
+    <section id="recent-posts-2" class="widget widget_recent_entries">
       <? /* get_template_part('catlist'); */ ?>
     </section>
   </aside>
@@ -67,3 +68,4 @@ get_header(); ?>
 
 <?php
 get_footer();
+
