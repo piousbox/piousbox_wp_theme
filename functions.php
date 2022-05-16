3f8fff3e83c496d715e263de533d0ea1ff1d65fa
piousbox <admin@piousbox.com>
2022-05-16 10:01:34 -0500

    style version bump

--- functions.php
+++ functions.php
@@ -110,7 +110,7 @@ function twentyseventeen_setup() {
 	 * This theme styles the visual editor to resemble the theme style,
 	 * specifically font, colors, and column width.
 	  */
-	add_editor_style( array( 'assets/css/editor-style.css?version=0.0.0', twentyseventeen_fonts_url() ) );
+	add_editor_style( array( 'assets/css/editor-style.css', twentyseventeen_fonts_url() ) );
 
 	// Define and register starter content to showcase the theme on new sites.
 	$starter_content = array(
@@ -437,7 +437,7 @@ function twentyseventeen_scripts() {
 	wp_enqueue_style( 'twentyseventeen-fonts', twentyseventeen_fonts_url(), array(), null );
 
 	// Theme stylesheet.
-	wp_enqueue_style( 'twentyseventeen-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'twentyseventeen-style', get_stylesheet_uri(), '', '0.0.0' ); // _vp_ 2022-05-16 style version bump
 
 	// Load the dark colorscheme.
 	if ( 'dark' === get_theme_mod( 'colorscheme', 'light' ) || is_customize_preview() ) {
